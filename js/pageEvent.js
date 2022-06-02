25a29fa57bb8575bac3ea347f6ff8ed3814f0e37
SueYeong <dudtb205@gmail.com>
2022-06-02 12:09:44 +0900

    page done

--- js/pageEvent.js
+++ js/pageEvent.js
@@ -1,4 +1,27 @@
 (function(){
 
+    const gageEl = document.querySelector('.gage')
+    const wrapEl = document.querySelector('.wrap')
+
+    const handleGage = () => {
+        const sct = window.pageYOffset
+        // gageEl.style.width = sct + "%"
+        // console.log('페이지 전체 높이값:', wrapEl.offsetHeight)
+        // console.log('브라우저의 높이값:', window.innerHeight)
+        // console.log('실제 사용할수 있는값:', wrapEl.offsetHeight - window.innerHeight)
+
+        const useScroll = wrapEl.offsetHeight - window.innerHeight
+        const per = sct / useScroll * 100 
+        console.log(per)
+
+        gageEl.style.width = per + "%"
+
+        // *공식
+        // =>사용할 스크롤값 : 문서의 높이값 - 브라우저의 높이값
+        //         (선택자.offsetHeight - window.innerHeight)
+        // =>사용할 스크롤 퍼센트 처리 : 사용할 스크롤 / 사용할수 있는 스크롤값 * 100 + "%"
+        //         (pageYOffset / 계산해둔 사용할 스크롤값 * 100)
+    }
+
     window.addEventListener('scroll', handleGage)
 })()
--- js/style.js
+++ js/style.js
@@ -4,5 +4,6 @@
     const boxEl = document.querySelector('.box')
 
     boxEl.style.height = "500px"
+    boxEl.style.borderRadius = "50%"
 
 })()
